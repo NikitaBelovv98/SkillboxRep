3cc42e386b4fe4a87eaf717c6a497b3b8c70f6ad
Nikita Tsybaev <cibaev.nikita@icloud.com>
2023-04-04 17:11:51 +0500

    add greeting

--- Desktop/m16/m16/ViewController.swift
+++ Desktop/m16/m16/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .darkGray
     }
+   
+    var greeting = "Hello World!"
 
 
 }

15bc0f5a5f997fd4a0ef8d78e50e5081ab867d2b
Nikita Tsybaev <cibaev.nikita@icloud.com>
2023-04-04 17:07:36 +0500

    background colour

---
+++ Desktop/m16/m16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  m16
+//
+//  Created by Никита Цыбаев on 04.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
